64416b5c3bee36c160ba3a3e2f0e9b2382edcb7c
Awadhesh Kumar <>
2018-06-08 18:26:46 +0530

    app.module.ts updated

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,18 +9,21 @@ import { File } from '@ionic-native/file';
 import { FileTransfer } from '@ionic-native/file-transfer';
 import { FilePath } from '@ionic-native/file-path';
 import { Camera } from '@ionic-native/camera';
+import { FileChooser } from '@ionic-native/file-chooser';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ReadmorelessPage } from '../pages/readmoreless/readmoreless';
 import { UploadimagePage } from '../pages/uploadimage/uploadimage';
+import { UploadvideoPage } from '../pages/uploadvideo/uploadvideo';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ReadmorelessPage,
-    UploadimagePage
+    UploadimagePage,
+    UploadvideoPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +35,8 @@ import { UploadimagePage } from '../pages/uploadimage/uploadimage';
     MyApp,
     HomePage,
     ReadmorelessPage,
-    UploadimagePage
+    UploadimagePage,
+    UploadvideoPage
   ],
   providers: [
     StatusBar,
@@ -41,6 +45,7 @@ import { UploadimagePage } from '../pages/uploadimage/uploadimage';
     FileTransfer,
     FilePath,
     Camera,
+    FileChooser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
